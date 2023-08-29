1222f150eec3cf6469475f28e3714bc29866c2a7
juanRiveraAlvarez <juanpariveraalvarez@gmail.com>
2023-08-28 22:22:21 -0700

    Intento5

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -97,13 +97,13 @@ public class TV {
     }
 
     public void volumenUp(){
-        if(this.estado && this.volumen>1 && this.volumen<7){
+        if(this.estado &&  this.volumen<7){
             this.volumen = this.volumen+1;
         }
     }
 
     public void volumenDown(){
-        if(this.estado && this.volumen>1 && this.volumen<7){
+        if(this.estado && this.volumen>1){
             this.volumen = this.volumen-1;
         }
     }
